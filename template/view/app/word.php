b830c965bf3a254c90e43458363795b7d1d28b70
chenjw <yuanzhumc@163.com>
2021-06-11 15:03:36 +0800

    修改

---
+++ template/view/app/word.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ${NAME}
+ * @project localhost
+ * @copyright 院主网络科技
+ * @author yuanzhumc
+ * @version 1.0
+ * @date: 2021/6/11
+ * @createTime 15:03
+ * @filename word.php
+ * @product_name PhpStorm
+ * @link https://github.com/yuanzhumc/localhost
+ * @example
+ */
+?>
+<h1>此功能正在开发中</h1>
